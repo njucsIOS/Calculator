6c382364932e4628ab6c6bd32c565f0a7299e9fa
SicenLiu <apple@114-212-134-182.nju.edu.cn>
2016-01-21 22:03:59 +0800

    汇率实现

--- Calculator/RateExchangeViewController.h
+++ Calculator/RateExchangeViewController.h
@@ -8,6 +8,36 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RateExchangeViewController : UIViewController
+@interface RateExchangeViewController : UIViewController<UITextFieldDelegate>{
+    NSMutableString *inputString_;
+    BOOL chooseFrom;
+}
+
+- (IBAction)FromSelect:(id)sender;
+- (IBAction)ToSelect:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UILabel *ToAmount;
+@property (weak, nonatomic) IBOutlet UILabel *FromAmount;
+
+@property (weak, nonatomic) IBOutlet UIButton *Zero;
+@property (weak, nonatomic) IBOutlet UIButton *One;
+@property (weak, nonatomic) IBOutlet UIButton *Two;
+@property (weak, nonatomic) IBOutlet UIButton *Three;
+@property (weak, nonatomic) IBOutlet UIButton *Four;
+@property (weak, nonatomic) IBOutlet UIButton *Five;
+@property (weak, nonatomic) IBOutlet UIButton *Six;
+@property (weak, nonatomic) IBOutlet UIButton *Seven;
+@property (weak, nonatomic) IBOutlet UIButton *Eight;
+@property (weak, nonatomic) IBOutlet UIButton *Nine;
+@property (weak, nonatomic) IBOutlet UIButton *Dot;
+@property (weak, nonatomic) IBOutlet UIButton *Clear;
+@property (weak, nonatomic) IBOutlet UIButton *Equal;
+@property (weak, nonatomic) IBOutlet UIButton *Delete;
+
+@property (weak, nonatomic) IBOutlet UILabel *FromContent;
+@property (weak, nonatomic) IBOutlet UILabel *ToContent;
+
+- (IBAction)OnBtnClick:(id)sender;
+
 
 @end
